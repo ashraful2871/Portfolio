205c1a1dbfba32abf9c373877c049480344d1235
ashraful2871 <ashrafulislam2758@gmail.com>
2025-02-13 02:17:23 +0600

    Configured GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routs.jsx";
+import router from "./routes/Routs.jsx";
 import { Toaster } from "react-hot-toast";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
--- src/routes/Routs.jsx
+++ src/routes/Routs.jsx
@@ -1,48 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import MainLAyout from "../Layout/MainLAyout";
 import Home from "../pages/Home";
 import ProjectDetails from "../pages/ProjectDetails";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import AboutMe from "../components/AboutMe";
-import ContactMe from "../components/ContactMe";
-import Education from "../components/Education";
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element: <MainLAyout></MainLAyout>,
+    element: <MainLAyout />,
     children: [
       {
         path: "/",
-        element: <Home></Home>,
+        element: <Home />,
       },
       {
         path: "/projectDetails/:id",
-        element: <ProjectDetails></ProjectDetails>,
-        // loader: ({ params }) =>
-        //   fetch(`${import.meta.env.VITE_API_URL}/projectDetails/${params.id}`),
-      },
-      {
-        path: "/skills",
-        element: <Skills></Skills>,
-      },
-      {
-        path: "/projects",
-        element: <Projects></Projects>,
-      },
-      {
-        path: "/about",
-        element: <AboutMe></AboutMe>,
-      },
-      {
-        path: "/contact",
-        element: <ContactMe></ContactMe>,
-      },
-      {
-        path: "/education",
-        element: <Education></Education>,
+        element: <ProjectDetails />,
       },
     ],
   },
 ]);
+
 export default router;
